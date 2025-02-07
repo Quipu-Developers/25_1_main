cf96330e4736bffbed4c58f4f336f441c5aab70e
YenaLey <yena.e121@gmail.com>
2025-02-08 00:12:25 +0900

    Feat: font 셋팅 추가

---
+++ frontend/src/types/type.d.ts
@@ -0,0 +1,5 @@
+declare interface Type {
+  item: BaseResponse;
+  setIsEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,15 +2,21 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Next.js 13+ App Router
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // 재사용 가능한 컴포넌트
+    "./src/hooks/**/*.{js,ts,jsx,tsx,mdx}", // 커스텀 훅 (필요할 경우)
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}", // 라이브러리 유틸 (필요할 경우)
+    "./public/fonts/fonts.ts",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        spoqaHanSansNeo: ["var(--font-spoqaHanSansNeo)", "sans-serif"],
+        firaCode: ["var(--font-fira-code)", "monospace"],
       },
     },
   },
