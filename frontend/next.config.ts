71b05557051d466a6b124ab2464e3c8115cb7a47
YenaLey <yena.e121@gmail.com>
2025-02-26 23:59:02 +0900

    Chore: next.config 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,11 @@ const nextConfig = {
   },
   images: {
     domains: [
-      "zippy.b-cdn.net",
-      "cdn.jsdelivr.net",
-      "pub-e688831beea1479f8b217d983b99b523.r2.dev",
-      "pub-880f96b9aa254fce88011c97e585d2bd.r2.dev",
+      "cdn.jsdelivr.net", // 기술 스택 이미지
+      "pub-e688831beea1479f8b217d983b99b523.r2.dev", // (퀴푸 Cloudflare)
+      "pub-880f96b9aa254fce88011c97e585d2bd.r2.dev", // (퀴푸 Cloudflare)
+      "zippy.b-cdn.net", // (이예나 개인 Cloudflare + BunnyCDN)
+      "pub-80a42cc7d41749078071917a4265d3ca.r2.dev", // (이예나 개인 Cloudflare)
     ],
   },
 };
